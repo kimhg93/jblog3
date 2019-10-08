e5b6eed7114012313473ea06c3ab94448b5c6fb6
박새얀 <@PARKSAEYAN>
2019-10-08 11:11:24 +0900

    init

---
+++ src/main/java/kr/co/itcen/jblog/controller/UserController.java
@@ -0,0 +1,16 @@
+package kr.co.itcen.jblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join() {		
+		System.out.println("test");
+		return "user/join";
+	}
+}
---
+++ src/main/java/kr/co/itcen/jblog/vo/UserVo.java
@@ -0,0 +1,34 @@
+package kr.co.itcen.jblog.vo;
+
+public class UserVo {
+	private String id;
+	private String name;
+	private String password;
+	private String reg_Date;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getReg_Date() {
+		return reg_Date;
+	}
+	public void setReg_Date(String reg_Date) {
+		this.reg_Date = reg_Date;
+	}	
+	
+}
