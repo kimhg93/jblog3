28007ce74bd5df1421f52eb4fdf3199aee27679b
박새얀 <@PARKSAEYAN>
2019-10-10 20:40:49 +0900

    인증

---
+++ src/main/java/kr/co/itcen/jblog/security/AdminInterceptor.java
@@ -0,0 +1,30 @@
+package kr.co.itcen.jblog.security;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import kr.co.itcen.jblog.vo.UserVo;
+
+public class AdminInterceptor extends HandlerInterceptorAdapter {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession(true);
+		UserVo authUser = (UserVo)session.getAttribute("authUser");
+		
+		Map pathVariables = (Map) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE); 
+		if(!pathVariables.get("id").equals(authUser.getId())) {
+			response.sendRedirect(request.getContextPath()+"/"+authUser.getId());
+			return false;
+		}
+		
+		return true;
+	}
+}
